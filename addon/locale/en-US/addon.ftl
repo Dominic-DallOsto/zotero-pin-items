f6269706328ef9eff44e62ab20377738ad1e8479
Dom <dominicd7@hotmail.com>
2024-11-24 19:55:44 +0100

    Add options for setting sort order of pinned items and standalone files/notes Fixes #2

--- addon/locale/en-US/addon.ftl
+++ addon/locale/en-US/addon.ftl
@@ -4,3 +4,19 @@ pin-item = Pin Item
 pin-items = Pin Items
 unpin-item = Unpin Item
 unpin-items = Unpin Items
+
+pref-title = Pin Items
+
+pref-pinitems-sort-order-title = Pinned Items Sort Order
+pref-pinitems-sort-order-description = How should items be sorted by the secondary column when pinned items are at the top? Note: items without a particular value are considered lower than all others.
+pref-pinitems-sort-order-ascending =
+    .label = Ascending order
+pref-pinitems-sort-order-descending =
+    .label = Descending order
+
+pref-pinitems-sort-order-files-title = Sort Order for Files and Standalone Notes
+pref-pinitems-sort-order-files-description = How should items that don't have Zotero parent items (eg. raw files and standalone notes) be sorted when pinned items are at the top?
+pref-pinitems-sort-order-files-before =
+    .label = Before regular items
+pref-pinitems-sort-order-files-after = 
+    .label = After regular items
--- src/modules/overlay.ts
+++ src/modules/overlay.ts
@@ -5,23 +5,49 @@ import {
 	setItemExtraProperty,
 	clearItemExtraProperty,
 } from "../utils/extraField";
+import {
+	getPref,
+	initialiseDefaultPref,
+	getPrefGlobalName,
+} from "../utils/prefs";
 
 const PIN_ITEMS_COLUMN_ID = "pinitems";
 const PINNED_EXTRA_FIELD = "Pinned_Collections";
+const PREF_SORT_ORDER = "pinitems-sort-order";
+enum SortOrder {
+	Ascending = 0,
+	Descending = 1,
+}
+
+const PREF_SORT_ORDER_FILES = "pinitems-sort-order-files";
+enum SortOrderFiles {
+	Before = 0,
+	After = 1,
+}
 
 export default class ZoteroPinItems {
 	pinItemsColumnId?: string | false;
 	preferenceUpdateObservers?: symbol[];
 
 	constructor() {
+		this.initialiseDefaultPreferences();
 		this.addPinItemsColumn();
 		this.addRightClickMenuItems();
 		this.addOverlayStyleSheet();
+		this.addPreferencesMenu();
+		this.addPreferenceUpdateObservers();
 	}
 
 	public unload() {
 		this.removePinItemsColumn();
 		this.removeRightClickMenuItems();
+		this.removePreferencesMenu();
+		this.removePreferenceUpdateObservers();
+	}
+
+	initialiseDefaultPreferences() {
+		initialiseDefaultPref(PREF_SORT_ORDER, SortOrder.Ascending);
+		initialiseDefaultPref(PREF_SORT_ORDER_FILES, SortOrderFiles.After);
 	}
 
 	addPinItemsColumn() {
@@ -34,8 +60,14 @@ export default class ZoteroPinItems {
 			pluginID: config.addonID,
 			dataProvider: (item: Zotero.Item, dataKey: string) => {
 				return item.isRegularItem()
-					? this.isItemPinned(item).toString()
-					: "";
+					? this.isItemPinned(item)
+						? "T"
+						: getPref(PREF_SORT_ORDER) == SortOrder.Ascending
+							? "Z" // ascending: Z comes after T so secondary column will be sorted ascending if pinned items are on top
+							: "F" // descending: F comes before T so secondary column will be sorted descending if pinned items are on top
+					: getPref(PREF_SORT_ORDER_FILES) == SortOrderFiles.Before
+						? "A" // "A" comes before "T", "F", or "Z"
+						: "ZZ"; // "ZZ" comes after "T", "F", or "Z"
 			},
 			renderCell: (
 				index: number,
@@ -46,7 +78,7 @@ export default class ZoteroPinItems {
 					`cell ${column.className}`,
 					"",
 				);
-				if (data === "true") {
+				if (data === "T") {
 					const icon = this.createSpanElement("icon icon-css", "");
 					icon.style.cssText +=
 						"background: url(chrome://zotero/skin/16/universal/pin.svg) content-box no-repeat center/contain; fill: var(--fill-secondary); -moz-context-properties: fill; width: 12px; height: 12px; padding: 1px; box-sizing: content-box;";
@@ -56,7 +88,6 @@ export default class ZoteroPinItems {
 				return cell;
 			},
 			fixedWidth: true,
-			// staticWidth: true,
 			width: "32",
 			zoteroPersist: ["width", "hidden", "sortDirection"],
 		});
@@ -214,5 +245,51 @@ export default class ZoteroPinItems {
 		link.rel = "stylesheet";
 		link.href = `chrome://${config.addonRef}/content/skin/default/overlay.css`;
 		window.document.documentElement.appendChild(link);
+	}
+
+	addPreferencesMenu() {
+		const prefOptions = {
+			pluginID: config.addonID,
+			src: rootURI + "chrome/content/preferences.xhtml",
+			label: getString("pref-title"),
+			image: `chrome://${config.addonRef}/content/icons/favicon.png`,
+			defaultXUL: true,
+		};
+		void Zotero.PreferencePanes.register(prefOptions);
+	}
+
+	removePreferencesMenu() {
+		Zotero.PreferencePanes.unregister(config.addonID);
+	}
+
+	addPreferenceUpdateObservers() {
+		this.preferenceUpdateObservers = [
+			Zotero.Prefs.registerObserver(
+				getPrefGlobalName(PREF_SORT_ORDER),
+				(value: string) => {
+					this.removePinItemsColumn();
+					this.addPinItemsColumn();
+				},
+				true,
+			),
+			Zotero.Prefs.registerObserver(
+				getPrefGlobalName(PREF_SORT_ORDER_FILES),
+				(value: string) => {
+					this.removePinItemsColumn();
+					this.addPinItemsColumn();
+				},
+				true,
+			),
+		];
+	}
+
+	removePreferenceUpdateObservers() {
+		if (this.preferenceUpdateObservers) {
+			for (const preferenceUpdateObserverSymbol of this
+				.preferenceUpdateObservers) {
+				Zotero.Prefs.unregisterObserver(preferenceUpdateObserverSymbol);
+			}
+			this.preferenceUpdateObservers = undefined;
+		}
 	}
 }
